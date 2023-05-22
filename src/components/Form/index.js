c60fe22383d5b00beb13d1c02e6f83b3736d848e
Lukas Weckner <lu.weckner@web.de>
2023-05-22 21:23:42 +0200

    create and style form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,32 @@
-import Input from "./../Input";
+import Button from "../Button";
+import "./Form.css";
 
 export default function Form() {
   return (
     <>
-      <h2>New Entry</h2>
-      <Input htmlFor="entry-title" id="entry-title" name="entry-title" />
+      <form className="form" aria-labelledby="title">
+        <h2 className="form__title" id="title">
+          New Entry
+        </h2>
+        <label htmlFor="entry-title">Motto</label>
+        <input
+          className="form__inputs"
+          htmlFor="entry-title"
+          id="entry-title"
+          name="entry-title"
+        />
+        <label className="form__content-label" htmlFor="entry-content">
+          Notes
+        </label>
+        <textarea
+          className="form__inputs"
+          rows={5}
+          htmlFor="entry-content"
+          id="entry-content"
+          name="entry-content"
+        ></textarea>
+        <Button>Create</Button>
+      </form>
     </>
   );
 }
--- src/components/Input/index.js
+++
@@ -1,8 +0,0 @@
-export default function Input({ name, id, htmlFor }) {
-  return (
-    <>
-      <label htmlFor={htmlFor}>Motto</label>
-      <input id={id} name={name}></input>
-    </>
-  );
-}
